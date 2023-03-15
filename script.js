e9c32f360034193b8bbb0c49ace558f60ed11a65
otrop <79354723+otrop@users.noreply.github.com>
2023-03-15 15:29:05 +0700

    Major change version
    change a lot of details to make it responsively.

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // When the user scrolls down 20px from the top of the document, slide down the navbar
 // When the user scrolls to the top of the page, slide up the navbar (50px out of the top view)
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-50px";
-  }
-}
+// window.onscroll = function() {scrollFunction()};
 
-const toggleButton = document.getElementsByClassName('toggle-botton')[0]
-const nacbarLinks = document.getElementsByClassName('menu')[0]
+//function scrollFunction() {
+//  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//    document.getElementById("navbar").style.top = "0";
+//  } else {
+//    document.getElementById("navbar").style.top = "-50px";
+//  }
+//}
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const nacbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-  nacbarLinks.classList.toggle('ative')
+  nacbarLinks.classList.toggle('active')
 })
